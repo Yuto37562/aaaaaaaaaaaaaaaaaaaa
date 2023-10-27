2362715acc3c241a45c403b18fa586a70bd45aa2
Yuto37562 <149153598+Yuto37562@users.noreply.github.com>
2023-10-27 19:21:23 +0900

    2023_10_23_kadai1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int a, b, sum;
+
+	printf("Please Enter Number:");
+	scanf_s("%d", &a);
+
+
+	printf("Please Enter Number:");
+	scanf_s("%d", &b);
+
+	sum = a + b;
+
+	printf("ŒvZŒ‹‰Ê‚ÍA%d‚Å‚·B", sum);
+
+	return 0;
+
+}
