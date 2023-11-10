99fdd623c564cebb86a37217a084760c27e499f7
Yuto37562 <149153598+Yuto37562@users.noreply.github.com>
2023-11-10 19:15:40 +0900

    2023_11_10_kadai1

---
+++ 2023_11_10_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void) {
+
+	for (int i = 1; i <= 10000; i++) {
+
+		if (i % 13 == 0) printf("%d\n",i);
+		else if (i % 17 == 0)printf("%d\n",i);
+
+	}
+
+	return 0;
+
+}
