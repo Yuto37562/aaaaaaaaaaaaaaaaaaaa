bb2d3f67d6c330427c303adbd5ab955974ef21f3
Yuto37562 <149153598+Yuto37562@users.noreply.github.com>
2023-11-10 19:28:03 +0900

    2023_11_10_kadai2

---
+++ 2023_11_10_2.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+
+int main(void) {
+
+	int max;
+
+	printf("最大値を入力してください。偶数のみを表示します。");
+
+	scanf_s("%d", &max);
+
+	for (int i = 1; i <= max; i++)
+	{
+		if (i % 2 == 0)printf("%d\n", i);
+	}
+	printf("表示しました。");
+	return 0;
+}
