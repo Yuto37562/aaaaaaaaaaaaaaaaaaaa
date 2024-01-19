156dc2d4e04a009cbc90694ecf736ec1c57ced6e
Yuto37562 <149153598+Yuto37562@users.noreply.github.com>
2024-01-19 18:08:32 +0900

    2024_01_19_kadai1途中

---
+++ 2024_1_19 17_47_19 (1).c
@@ -0,0 +1,40 @@
+/* main.c */ 
+#include <stdio.h>
+
+/* 基本に忠実なFizzBuzz */
+int main(void)
+{
+    int a;
+    int b;
+    int c;
+
+    printf("aの値を入力してください。");
+    scanf("%d", &a);
+    while (a==0){
+        printf("もう一度0以外の数字を入力してください。");
+        scanf("%d", &a);
+    }
+    printf("bの値を入力してください。");
+    scanf("%d",&b)
+    
+    printf("cの値を入力してください。");
+    scanf("%d",&c)
+
+    printf("%d,%d,%d",a,b,c);
+	
+	printf
+	
+	
+	//計算式作る
+	
+	
+	
+	
+	
+	
+	
+	
+
+    return 0;
+}
+
